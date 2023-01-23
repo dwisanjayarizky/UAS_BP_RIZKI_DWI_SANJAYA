bf213cc67e031d2a817ecfb4d09556ac65528774
Rizki_Dwi_Sanjaya <114788012+dwisanjayarizky@users.noreply.github.com>
2023-01-23 16:34:04 +0700

    Create function.py

---
+++ NO1/function.py
@@ -0,0 +1,16 @@
+#fungsi
+print("--------------------------")
+print("NAMA  :RIZKI DWI SANJAYA ")
+print("NIM   :20210801091 ")
+print("--------------------------")
+kota, provinsi = 'Tangerang', 'Banten'
+
+def hello ():
+  provinsi = 'Banten'
+  print(kota, provinsi)
+
+print('[PANGGIL FUNGSI hello()]')
+hello()
+
+print('\n[SECARA LANGSUNG]')
+print(kota, provinsi)
